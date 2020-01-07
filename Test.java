275d5ceb8fd947c01b76b39b73dccacd4d298593
PoojaKokilXor <59598210+PoojaKokilXor@users.noreply.github.com>
2020-01-07 13:07:21 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.sepg.ibase.common;
+
+public class Constants {
+
+	public static final String STORY_STATUS_OPEN = "open";
+	public static final String STORY_STATUS_DEV_IN_PROG = "Development in Progress";
+	public static final String STORY_STATUS_PROGRESS = "Progress";
+	
+	public static final String JIRA_URL_SUFFIX = "/rest/api/2/search?jql=";
+	public static final String JIRA = "JIRA";
+	public static final String ALL_TASKS = "ALL_TASKS";
+	public static final String DEFECT = "DEFECT";
+	
+}
